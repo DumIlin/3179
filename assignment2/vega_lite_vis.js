b5151b3fb2140d888293b34d2be3740973837ee9
DumIlin <111404925+DumIlin@users.noreply.github.com>
2022-10-17 18:39:35 +1100

    Delete vega_lite_vis.js

--- assignment2/vega_lite_vis.js
+++
@@ -1,9 +0,0 @@
-var vg_1 = "happines_map.json";
-vegaEmbed("#bar_chart", vg_1).then(function(result) {
-// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
-
-var vg_2 = "happines_gdp.json";
-vegaEmbed("#bubble", vg_2).then(function(result) {
-// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
